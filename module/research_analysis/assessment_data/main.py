2f333e4ad5df6b6b5ca37ee933bada4177d7e6d7
Paggie <paggie70424@gmail.com>
2025-07-08 17:07:57 +0930

    update code

--- module/research_analysis/assessment_data/main.py
+++ module/research_analysis/assessment_data/main.py
@@ -9,8 +9,11 @@ sys.path.append(research_analysis_dir)
 from common_function import read_single_csv_from_s3
 
 arn = "arn:aws:s3:::engage-ai-dataset"
-file_key = "term2405_course3547_alllog.csv"
+file_key_log = "term2405_course3547_alllog.csv"
+file_key_finalgrade = "allterm_course163601_finalgrade.csv"
 
-df = read_single_csv_from_s3(arn, file_key)
+df_log = read_single_csv_from_s3(arn, file_key_log)
+df_finalgrade = read_single_csv_from_s3(arn, file_key_finalgrade )
 
-print(df.head())
+print(df_log.head())
+print(df_finalgrade.head())
